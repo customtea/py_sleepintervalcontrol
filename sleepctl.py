6d98a999972351c885852ba59ee1a21250ddd448
customtea <40118676+customtea@users.noreply.github.com>
2024-01-16 14:55:25 +0900

    feat; another implemant

--- sleepctl.py
+++ sleepctl.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 
 __author__ = 'customtea (https://github.com/customtea/)'
 __version__ = '1.5.0'
@@ -13,7 +14,6 @@ class SleepControl:
             interval sec, by default 1
         """
         self.interval = interval
-        self.count = 0
         self.sleep_time = 0
         self.init_time = time.time()
 
@@ -30,7 +30,7 @@ class SleepControl:
             diff_time = func_end_time - func_start_time
             self.sleep_time = self.interval - diff_time - correction_time
             if self.sleep_time < 0:
-                print(f"[Warning] Processing Time ({diff_time}) is Overflowed Interval ({self.interval})")
+                print(f"{datetime.datetime.now().isoformat()} [Warning] Processing Time ({diff_time}) is Overflowed Interval ({self.interval})")
                 # print(f"Next Sleep Time Set at ({self.interval})'")
                 self.sleep_time = abs(self.sleep_time) % self.interval
             calc_end_time = time.time()
@@ -40,3 +40,17 @@ class SleepControl:
             return r
         return decorator_wrapper
 
+
+    def interval_func(self, func):
+        def decorator_wrapper(*args, **kwargs):
+            time.sleep(self.sleep_time)
+            
+            r = func(*args, **kwargs)
+
+            calc_start_time = time.time()
+            self.sleep_time = self.interval - (calc_start_time - self.init_time) % self.interval
+            calc_end_time = time.time()
+
+            self.sleep_time -=  (calc_end_time - calc_start_time)
+            return r
+        return decorator_wrapper
